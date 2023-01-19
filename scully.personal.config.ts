d4462418e07453112f89c9f5ef5d3d0094324f1f
Armen Vardanyan <armenvardanyan95@gmail.com>
2023-01-19 16:08:54 +0400

    Switch to pnpm

--- scully.personal.config.ts
+++ scully.personal.config.ts
@@ -3,6 +3,7 @@ import { ScullyConfig } from '@scullyio/scully';
 /** this loads the default render plugin, remove when switching to something else. */
 
 
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "personal",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
 import { MarkdownModule } from 'ngx-markdown';
 
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SanitizerPipe } from './common/pipes/dom-sanitizer.pipe';
 import { ArticlesComponent } from './pages/articles/articles.component';
+import { BlogPostComponent } from './pages/blog-post/blog-post.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { CodeSnippetsComponent } from './pages/code-snippets/code-snippets.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { OpenSourceComponent } from './pages/open-source/open-source.component';
 import { PodcastsComponent } from './pages/podcasts/podcasts.component';
 import { PublicTalksComponent } from './pages/public-talks/public-talks.component';
-import { SanitizerPipe } from './common/pipes/dom-sanitizer.pipe';
-import { CoursesComponent } from './pages/courses/courses.component';
-import { CodeSnippetsComponent } from './pages/code-snippets/code-snippets.component';
-import { OpenSourceComponent } from './pages/open-source/open-source.component';
-import { ScullyLibModule } from '@scullyio/ng-lib';
-import { BlogComponent } from './pages/blog/blog.component';
-import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import { BlogPostComponent } from './pages/blog-post/blog-post.component';
   ],
   providers: [
     MatIconRegistry,
+    {provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
 })
