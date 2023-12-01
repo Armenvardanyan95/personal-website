99e39588d1cbdede6e01c8aac751be792029f75b
Armen Vardanyan <armenvardanyan95@gmail.com>
2023-12-01 13:44:50 +0400

    Changes

--- src/app/pages/code-snippets/code-snippets.component.ts
+++ src/app/pages/code-snippets/code-snippets.component.ts
@@ -7,7 +7,30 @@ import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-code-snippets',
-    templateUrl: './code-snippets.component.html',
+    template: `
+    <div class="container">
+  <h1>Code Snippets</h1>
+  <p>
+    Here I present interesting use cases with short snippets of code that solve
+    those problems
+  </p>
+
+  @for (snippet of snippets; track snippet) {
+  <mat-card>
+    <mat-card-header>
+      <mat-card-title>{{ snippet.title }}</mat-card-title>
+      <mat-card-subtitle>
+        {{ snippet.description }}
+      </mat-card-subtitle>
+    </mat-card-header>
+    <mat-card-content>
+      <markdown [src]="snippet.path" lineNumbers></markdown>
+    </mat-card-content>
+  </mat-card>
+}
+
+</div>
+    `,
     styleUrls: ['./code-snippets.component.scss'],
     standalone: true,
     imports: [NgFor, MatCardModule, MarkdownModule]
