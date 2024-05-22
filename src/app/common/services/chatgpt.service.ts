75cbeb9b59eacf6a8e0aceb386134608053f3130
Armen Vardanyan <armenvardanyan95@gmail.com>
2024-05-22 14:13:23 +0400

    Remove the chatgpt references

--- src/app/common/services/chatgpt.service.ts
+++ src/app/common/services/chatgpt.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
 
 @Injectable({ providedIn: 'root' })
 export class ChatGPTService {
-  private readonly openai = new OpenAIApi(
-    new Configuration({
-      apiKey: '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',
-    })
-  );
+  private readonly openai = null;
+  // new OpenAIApi(
+  //   new Configuration({
+  //     apiKey: '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',
+  //   })
+  // );
 
   async runCompletion(prompt: string) {
-    const response = await this.openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {content: prompt, role: 'system', name: 'Armen', },
-      ],
-      max_tokens: 3_000,
-    });
+    // const response = await this.openai.createChatCompletion({
+    //   model: 'gpt-3.5-turbo',
+    //   messages: [
+    //     {content: prompt, role: 'system', name: 'Armen', },
+    //   ],
+    //   max_tokens: 3_000,
+    // });
 
-    return response.data.choices[0].message;
+    return 'response.data.choices[0].message; return'; 
   }
 }
